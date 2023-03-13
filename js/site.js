eb40408653f9b6d5a47d236c3a3678bffb763bdb
ShawnLSmith <shawn@rsatechnologyservices.com>
2023-03-13 12:33:32 -0500

    Tested array outputs in getValues and calculateMortgage. best known location for loanArray unknown. Issue persists.

--- js/site.js
+++ js/site.js
@@ -1,10 +1,10 @@
-function getValues(){
+function getValues() {
     //Get User Values from the DOM.
     let loanAmount = document.getElementById("loanAmount").value;
     let termMonths = document.getElementById("termMonths").value;
     let interestRate = document.getElementById("interestRate").value;
-    
-    //loanArray Declaration.
+
+    //loanArray Declaration here?.
     let loanArray = [];
 
     //Input validation for integers.
@@ -16,31 +16,44 @@ function getValues(){
     if (Number.isInteger(loanAmount) && Number.isInteger(termMonths) && Number.isInteger(interestRate)) {
 
         //Call calculateMortgage.
-        calculateMortgage(loanAmount, termMonths, interestRate);
+        loanArray = calculateMortgage(loanAmount, termMonths, interestRate);
+
 
         //Call displayData to Display Collected Data.
         displayData(loanArray);
     }
 
-    else
-    {
+    else {
         alert("You must enter an integer in the text boxes!")
     }
-        
-    
+
+
 }
-// function calculateMortgage(loanAmount, termMonths, interestRate){
-    
-// let totalMnthly = (loanAmount) * (interestRate/1200)/(1 - ( 1+ interestRate/1200)**(termMonths*-1));
-// // Math.pow( 1+ interestRate/1200),termMonths*-1))
-// let txtTotMnthly = "$ " . totalMnthly.toString();
-// document.getElementById("tprinciple").innerText() =txtTotMnthly;
-// }
+function calculateMortgage(loanAmount, termMonths, interestRate) {
 
-//function 
+    //loanArray here?
+    //let loanArray = [];
 
- function displayData (loanArray) {
-    
+     let totalMonthly = (loanAmount) * (interestRate/1200)/(1 - ( 1 * interestRate/1200)**(termMonths*-1));
+    // // Math.pow( 1+ interestRate/1200),termMonths*-1))
+    // let txtTotMonthly = "$ " . totalMonthly.toString();
+    // document.getElementById("tprinciple").innerText() =txtTotMonthly;
+
+    // loanArray [0] = '11111Hithere';
+    // loanArray [1] = "Hi%20there";
+    // loanArray [2] = "Hi&nbsp;there";
+    // loanArray [3] = "Hi&nbsp;there";
+    // loanArray [4] = "123423452345.478437"; 
+
+    //console.log("Loan Array display log");
+
+    //return (loanArray);
+
+    return (totalMonthly);
+}
+
+function displayData(loanArray) {
+
     //Get the table body element from the page.
     let tableBody = document.getElementById("results");
 
@@ -48,33 +61,33 @@ function getValues(){
     let templateRow = document.getElementById("loanTableTemplate");
 
     //Clear Table first
-    tableBody.innerHTML = "";
+    //tableBody.innerHTML = "";
 
-    for (let index = 0; index < loanArray.length; index += 5) {
+    for (let index = 0; index < loanArray; index += 5) {
 
         //importNode, new!! 9/23/2022
         let tableRow = document.importNode(templateRow.content, true);
 
         //Grab row tds from "Template Section" and put them into the array 
-        let rowCols = tableRow.querySelectorAll("td");  
+        let rowCols = tableRow.querySelectorAll("td");
 
         //Array Sets
-        rowCols[0].classList.add (loanArray[index]);
+        rowCols[0].classList.add(loanArray[index]);
         rowCols[0].textContent = loanArray[index];
 
-        rowCols[1].classList.add (loanArray[index + 1]);
+        rowCols[1].classList.add(loanArray[index + 1]);
         rowCols[1].textContent = loanArray[index + 1];
 
-        rowCols[2].classList.add (loanArray[index + 2]);
+        rowCols[2].classList.add(loanArray[index + 2]);
         rowCols[2].textContent = loanArray[index + 2];
 
-        rowCols[3].classList.add (loanArray[index + 3]);
+        rowCols[3].classList.add(loanArray[index + 3]);
         rowCols[3].textContent = loanArray[index + 3];
 
-        rowCols[4].classList.add (loanArray[index + 4]);
-        rowCols[4].textContent = loanArray[index + 4];       
-        
+        rowCols[4].classList.add(loanArray[index + 4]);
+        rowCols[4].textContent = loanArray[index + 4];
+
         tableBody.appendChild(tableRow);
     }
-    
+
 }
